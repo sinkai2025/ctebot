b6fa97924db7305d2f1a7f114bb0a8128047354a
sinkai2025 <x2004tom@cte.com.tw>
2025-06-10 15:42:35 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+import os
+import tempfile
+from flask import Flask, request, abort
+from dotenv import load_dotenv
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, ImageMessage, TextSendMessage
+
+load_dotenv()
+app = Flask(__name__)
+line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
+handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers.get("X-Line-Signature", "")
+    body = request.get_data(as_text=True)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+@handler.add(MessageEvent, message=ImageMessage)
+def handle_image(event):
+    message_content = line_bot_api.get_message_content(event.message.id)
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".jpg") as tf:
+        tf.write(message_content.content)
+        image_path = tf.name
+
+    # 模擬辨識保麗龍
+    label = "保麗龍"
+    suggestion = "這是保麗龍，請丟入塑膠類回收桶。"
+
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=f"{label}\n{suggestion}")
+    )
+
+if __name__ == "__main__":
+    app.run()
