cb086c6d6badcac21dcc23fa9b85dbb7d3807eba
sinkai2025 <x2004tom@cte.com.tw>
2025-06-11 14:54:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -43,4 +43,4 @@ def handle_image(event):
     line_bot_api.reply_message(event.reply_token, messages)
 
 if __name__ == "__main__":
-    app.run()
+   app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
